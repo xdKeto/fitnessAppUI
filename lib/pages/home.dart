6a839c31900d56e1a19055feffbcf9b340a14980
Yestoya Minggus <yestoya.minggus@gmail.com>
2024-09-27 22:11:50 +0700

    search field done

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,42 +8,68 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appbar(),
+      backgroundColor: Colors.white,
       body: Column(
         children: [
-          Container(
-            margin: EdgeInsets.only(top: 40, left: 40, right: 20),
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.11),
-                  blurRadius: 40,
-                  spreadRadius: 0.0
-                )
-              ]
-            ),
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                contentPadding: EdgeInsets.all(15),
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: SvgPicture.asset('assets/icons/search.svg'),
-                ),
-                suffixIcon: Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: SvgPicture.asset('assets/icons/filter.svg'),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  borderSide: BorderSide.none
-                )
-              ),
-            ),
-          )
+          searchField()
         ],
       ),
     );
+  }
+
+  Container searchField() {
+    return Container(
+          margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.11),
+                blurRadius: 40,
+                spreadRadius: 0.0
+              )
+            ]
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: const EdgeInsets.all(15),
+              hintText: "Search Pancakes",
+              hintStyle: const TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+              ),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset('assets/icons/search.svg'),
+              ),
+              suffixIcon: SizedBox(
+                width: 100,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const VerticalDivider(
+                        color: Colors.black,
+                        thickness: 0.2,
+                        indent: 10,
+                        endIndent: 10,
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.all(12),
+                          child: SvgPicture.asset('assets/icons/filter.svg'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none
+              )
+            ),
+          ),
+        );
   }
 }
 
@@ -63,11 +89,10 @@ AppBar appbar(){
               alignment: Alignment.center,
               decoration: BoxDecoration(
                   color: Colors.white, borderRadius: BorderRadius.circular(10)),
-              child: SvgPicture.asset(
-                'assets/icons/arrow-left.svg',
-                height: 20,
-                width: 20,
-              )),
+              child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: SvgPicture.asset('assets/icons/arrow-left.svg'),
+                  )),
         ),
         actions: [
           GestureDetector(
@@ -79,11 +104,10 @@ AppBar appbar(){
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(10)),
                 width: 37,
-                child: SvgPicture.asset(
-                  'assets/icons/dots.svg',
-                  height: 20,
-                  width: 20,
-                )),
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: SvgPicture.asset('assets/icons/dots.svg'),
+                  )),
           )
         ],
       );
